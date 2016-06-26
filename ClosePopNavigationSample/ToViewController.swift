1dfac251184656db84f9447866fd93d7f7f05778
TakahashiIkki <new_ittyandesu@outlook.jp>
2016-06-26 12:34:16 +0900

    戻るボタンを押した時に、アラートを出して何か行った後、前画面に戻るような実装。

--- ClosePopNavigationSample/ToViewController.swift
+++ ClosePopNavigationSample/ToViewController.swift
@@ -20,4 +20,24 @@ class ToViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onTouchReturnButton(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "アラート表示", message: "現在の画面を閉じますか？", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let defaultAction = UIAlertAction(title: "閉じる", style: .Default, handler: {
+            (action: UIAlertAction!) -> Void in
+            print("閉じる touch")
+            self.navigationController?.popViewControllerAnimated(true)
+        })
+        
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .Cancel, handler: nil)
+        
+        alert.addAction(defaultAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
